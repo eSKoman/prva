63a7cd8884444d83547e3cf6a01f393712baf28b
Slavomír Kotlár <57029442+eSKoman@users.noreply.github.com>
2022-05-01 13:39:05 +0200

    11.3.1 - Zadanie - jednoduchá kalkulačka

---
+++ zadania/javascript/js/script.js
@@ -0,0 +1,28 @@
+cislo1 = 4;
+cislo2 = 2;
+
+vypocitaj(cislo1, cislo2);
+document.write("<br>");
+vypocitaj(10, 5);
+
+
+function vypocitaj(cislo1, cislo2) {
+    document.write("Prvé číslo: " + cislo1);
+    document.write("<br>Druhé číslo: " + cislo2 + "<br>");
+    document.write("<br>Vykonávam matematické operácie:<br>");
+
+    if (cislo2 == 0){
+        document.writeln("<br>Delit nulou nejde!");
+    } else {
+        podiel = cislo1 / cislo2;
+        document.write("<br>" + cislo1 + " / " + cislo2 + " = " + podiel);
+    }
+    
+    rozdiel = cislo1 - cislo2;
+    sucin = cislo1 * cislo2;
+    sucet = cislo1 + cislo2;
+
+    document.write("<br>" + cislo1 + " + " + cislo2 + " = " + sucet);
+    document.write("<br>" + cislo1 + " * " + cislo2 + " = " + sucin);
+    document.write("<br>" + cislo1 + " - " + cislo2 + " = " + rozdiel + "<br>");
+}
